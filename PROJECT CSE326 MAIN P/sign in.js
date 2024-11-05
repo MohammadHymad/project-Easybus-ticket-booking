5f0c14669b6c52775e18b7b5b715ad74d2f07750
MohammadHymad <hymadchinni@hotmail.com>
2024-11-05 23:01:54 +0530

    Add files via upload

---
+++ PROJECT CSE326 MAIN P/sign in.js
@@ -0,0 +1,5 @@
+// Add any functionality as needed, e.g., form submission handling
+document.querySelector('.next-btn').addEventListener('click', () => {
+    alert('Next button clicked');
+  });
+  
